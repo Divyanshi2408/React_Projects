3c8cbdfcd5aa8a672d6ec555643c3d912621d65d
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-01-23 15:45:04 +0530

    updte

--- image-gallery/src/components/ImageGallery.jsx
+++ image-gallery/src/components/ImageGallery.jsx
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 import { im1, im7, im2, im3, im4, im5, im6, im, im8 } from '../Assets/images';
 import './ImageGallery.css';
 
-const images = [im1, im5, im2, im6, im, im7, im3, im4, im8];
+const images = ['https://media.craiyon.com/2023-07-11/0082c70c5e9b49c5b9f0d8d98e085bf0.webp', 'https://www.renderhub.com/squir/pikachu-pokemon/pikachu-pokemon-01.jpg', 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/90af2477095215.5c7dad740f730.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw3BDKosvHIXxfFSIV0BeZVq1UtUP_ru7W4w&s', 'https://www.zbrushcentral.com/uploads/default/original/4X/6/c/0/6c063046bc0973554c96d6bf04ff0521c948dc5e.jpeg', 'https://dl2.myminifactory.com/object-assets/5f7c6ef230f1d/images/720X720-2021-03-13-09h00m01.jpg','https://img.freepik.com/premium-photo/cute-round-blue-cat-character-with-big-eyes-happy-smile_1151-153227.jpg','https://img.freepik.com/premium-photo/adorable-3d-illustration-cute-pink-blue-pokemon-character-with-big-eyes_1151-152797.jpg','https://thangs.com/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-thangs-public%2Fuploads%2Fattachments%2Ff4985fd7-64bc-4feb-a00f-b53248b0b828%2Funtitled.184.png&w=3840&q=75','https://preview.free3d.com/img/2018/01/2158635337679636417/2lfrktxr.jpg', 'https://mir-s3-cdn-cf.behance.net/projects/404/88c776175433921.Y3JvcCwxMDMxLDgwNiw0NDksMjE0.png', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF7tRGhUbfDX8zTIThnak91Ocq65n3kqqHB0aOlYlpf_e5q_xYOqLFVco2AKTbkLmcdmA&usqp=CAU',];
 
 const ImageGallery = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
